42ccc9b7db52bd29fdd7d1454552a835e890a776
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2020-09-29 14:46:58 -0500

    Implementando nuestra capa de Servicios

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const app = express();
 const productsRouter = require("./routes/products");
 const productsApiRouter = require("./routes/api/products");
+const bodyParser = require("body-parser");
 
 /** Static */
 app.use(express.static(path.join(__dirname, "public")));
@@ -15,6 +16,8 @@ app.use("/products", productsRouter);
 
 /** Route API */
 app.use("/api/products", productsApiRouter);
+
+app.use(bodyParser.json());
 
 const server = app.listen(8000, function () {
   console.log(`listening in http://localhost:${server.address().port}`);
